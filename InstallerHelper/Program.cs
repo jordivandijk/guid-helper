e1a4198f273b5480a1496b6b843e12037e10c424
jordivandijk <jordivandijk05@gmail.com>
2023-12-22 12:19:47 +0100

    Added installer helper for adding guidfolder to path variable

---
+++ InstallerHelper/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string? oldPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+string programFiles = Environment.ExpandEnvironmentVariables("%ProgramW6432%");
+
+if (!string.IsNullOrEmpty(oldPath))
+{
+    Environment.SetEnvironmentVariable("PATH", oldPath + $"{programFiles}\\JordIT\\guid\\", EnvironmentVariableTarget.Machine);
+}
+else
+{
+    Console.WriteLine("Something went wrong while adding the program to the PATH variables");
+    Console.ReadLine();
+}
